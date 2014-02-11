276045e7e1bf175c695a8b19efa885a742f6fd42
Ariel Allon <arielallon@gmail.com>
2014-02-11 06:38:02 -0600

    Added lots of comments. Renamed variables for clarity.

--- Juggler.php
+++ Juggler.php
@@ -4,30 +4,49 @@ require_once('Interface.php');
 
 /**
  *  Option A in the book.
+ *  Picks up the first element and starts shifting elements left $shiftAmount places, skipping the ones in between.
+ *  When it hits the original spot, it drops the first element in its new spot.
+ *  If we haven't shifted everything yet, it picks up the next element and repeates the process.
  **/
 class Juggler implements VectorRotate
 {
-	public function rotate(&$array, $shift)
+	/**
+	 *  Note: Currently left shift only
+	 **/
+	public function rotate(&$array, $shiftAmount)
 	{
 		$vectorLength = count($array);
-		$count = $m = 0;
-
+		
+		$count = $offset = 0;
 		// Potentially not necessary.
-		$shift = $shift % $vectorLength;
+		$shiftAmount = $shiftAmount % $vectorLength;
 
+		// Make sure to stop after we've moved each element once. 
+		// (Technically, after count($array) moves, which doesn't guarantee a single element isn't moved twice. 
+		// However, the algorithm will only hit each element once.)
 		while ($count < $vectorLength) {
-			$i = $shift + $m;
-			$t = $array[$m];
+			$index = $shiftAmount + $offset;
 
-			while ($i % $vectorLength != $m) {
-				$array[($i - $shift) % $vectorLength] = $array[$i % $vectorLength];
-				$i += $shift;
+			// Pick up the temp element
+			$tempElement = $array[$offset];
+
+			// Loop until we've hit our starting spot again for this offset
+			while ($index % $vectorLength != $offset) {
+				
+				// shift an element left $shiftAmount spots
+				$array[($index - $shiftAmount) % $vectorLength] = $array[$index % $vectorLength];
+
+				// the next element to shift will be $shiftAmount spaces to the right of here
+				$index += $shiftAmount;
+
 				$count++;
 			}
 
-			$array[($i - $shift) % $vectorLength] = $t;
+			// Drop the temp element in its new spot
+			$array[($index - $shiftAmount) % $vectorLength] = $tempElement;
+
 			$count++;
-			$m++;
+			$offset++;
 		}
 	}
 }
